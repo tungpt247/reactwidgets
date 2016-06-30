22cb6cb206e5876ec681b1386d72a0d69e202833
Tung Phan <tungpt247@gmail.com>
2016-06-30 15:08:55 +0700

    Add Alert component sample

--- src/components/Alert.js
+++
@@ -1,8 +0,0 @@
-import React, { Component } from 'react'
-
-class Alert extends Component {
-  render() {
-    return <div>Alert</div>
-  }
-}
-export default Alert
---
+++ src/components/alert/Alert.js
@@ -0,0 +1,11 @@
+import React from 'react'
+require('./alert.scss')
+
+class Alert extends React.Component {
+
+  render() {
+    return <div className="alert">Alert</div>
+  }
+}
+
+export default Alert
